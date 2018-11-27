d62262db9f9846f4b39abffd482c3e044dfab30d
Murad Jafarov <mjafarov@gmail.com>
2018-11-27 12:31:58 +0400

    Made dev and master different

--- app/tests/app-test.py
+++ app/tests/app-test.py
@@ -9,7 +9,6 @@ class MyTest(unittest.TestCase):
                 self.assertEqual(my_function(0, 0), 0)
                 self.assertEqual(my_function(-1, -1), -2)
                 self.assertEqual(my_function(1.0, 1), 2)
-                self.assertEqual(my_function(1.1, 1.1), 2.2)
 
 
 if __name__ == '__main__':
