b6da92e6ddf28ee57788ba00264719f0dfe872ba
Murad Jafarov <mjafarov@gmail.com>
2018-11-27 11:54:41 +0400

    Removed a wrong test case

--- app/tests/app-test.py
+++ app/tests/app-test.py
@@ -10,7 +10,6 @@ class MyTest(unittest.TestCase):
                 self.assertEqual(my_function(-1, -1), -2)
                 self.assertEqual(my_function(1.0, 1), 2)
                 self.assertEqual(my_function(1.1, 1.1), 2.2)
-                self.assertEqual(my_function(1, 1), 4)
 
 
 if __name__ == '__main__':
